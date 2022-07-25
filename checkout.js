5a73883522cef5b89ffc3fec4ee60c21c2095b7f
Bruno Moreira <ok.computador@gmail.com>
2022-07-25 14:56:44 -0300

    Funcionalidades adicionadas

--- checkout.js
+++ checkout.js
@@ -11,26 +11,57 @@ let _opcaoParcela = document.querySelectorAll(".parcelas");
 let _opcoesPagamento = document.querySelectorAll(".OpcoesPagamento");
 let _qtdParcelas = document.querySelectorAll(".qtdParcelas");
 let _botaoFinalizar = document.querySelector(".botaoFinalizar");
+let _cardCupom = document.querySelectorAll(".cardCupom");
 let valorTotal = 0;
-let valorSubTotal = 285.0;
+let valorSubTotal = 508.0;
+let valorFrete = 0;
+let prazoFrete = '';
 let quantidadeParcelas = 0;
+let desconto = 0;
+let formaPagamento = '';
+let tipoParcelamento = "";
+const lockScreen = screen.orientation.lock('portrait');
+
+function atualizaResumo(parametro, valor){
+
+  valorTotal = parseFloat((valorSubTotal * (1 - (desconto/100))) + valorFrete).toFixed(2);
+
+  if(parametro == 'subtotal')
+  return;
+
+  if(parametro == 'frete')
+  _frete.innerHTML = valor;
+
+  if(parametro == 'total'){
+   _valorTotal.innerHTML = valor; 
+   _valorTotalResumo.innerHTML = valor;
+  }
+  
+  if(parametro == 'prazo')
+  _prazoEntrega.innerHTML = valor;
+
+  if(parametro == 'pagamento')
+  _pagamento.innerHTML = valor;
+
+  if(parametro == 'parcelas')
+  _parcelas.innerHTML = valor;
+
+}
 
 function adicionaFrete(valor, prazo) {
   valorTotal = valorSubTotal + valor;
   valorTotal = valorTotal.toFixed(2);
-  let prazoFrete = `Até ${prazo} dias úteis`;
-  let valorFrete = `R$ ${valor},00`;
+  prazoFrete = `Até ${prazo} dias úteis`;
+  valorFrete = `R$ ${valor},00`;
   if (prazo === 0) prazoFrete = `Imediato`;
 
-  _valorTotal.innerHTML = `R$ ${valorTotal}`;
-  _valorTotalResumo.innerHTML = `R$ ${valorTotal}`;
-  _prazoEntrega.innerHTML = prazoFrete;
-  _frete.innerHTML = `${valorFrete}`;
+  atualizaResumo('total', `R$ ${valorTotal}`);
+  atualizaResumo('prazo', prazoFrete);
+  atualizaResumo('frete',`${valorFrete}`);
 }
 
 function selecionaPagamento(pagamento, parcelas) {
-  let tipoParcelamento = "";
-
+  
   if (typeof parcelas === "number") {
     tipoParcelamento = calcParcelamento(parcelas);
     quantidadeParcelas = parcelas;
@@ -41,29 +72,15 @@ function selecionaPagamento(pagamento, parcelas) {
   if (parcelas === "Aguardando definir" && quantidadeParcelas > 0)
     tipoParcelamento = calcParcelamento(quantidadeParcelas);
 
-  _pagamento.innerHTML = pagamento;
-  _parcelas.innerHTML = tipoParcelamento;
+    atualizaResumo('pagamento', pagamento);
+    atualizaResumo('parcelas', tipoParcelamento);
   if (parcelas !== "Aguardando definir") mostraFinalizar();
 }
 
 function calcParcelamento(parcelas) {
-  let valores = (valorTotal / parcelas).toFixed(2);
+  let valores = parseFloat((valorTotal / parcelas).toFixed(2)).toFixed(2);
   let texto = `${parcelas} x de R$ ${valores}`;
   return texto;
-}
-
-function changeScreen() {
-  // _checkout.setAttribute("class", "hidden");
-}
-
-function fechaSacola() {
-  _fog.classList.remove("mostrar__opacidade");
-  _sacola.classList.remove("mostrar__slide");
-}
-
-function abreSacola() {
-  _fog.classList.add("mostrar__opacidade");
-  _sacola.classList.add("mostrar__slide");
 }
 
 function mostraParcelas() {
@@ -90,6 +107,13 @@ function mostraPagamentos() {
 
 function mostraFinalizar() {
   _botaoFinalizar.classList.add("botao__show");
+}
+
+function mostrarCupom(){
+  let quantidade = _cardCupom.length;
+
+  for(let i = 0; i < quantidade; i++)
+  _cardCupom[i].classList.add("cupom__show");
 }
 
 function defineParcelamento() {
